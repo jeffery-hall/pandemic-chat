00c53150bc61156cc8de70c227b33bd9d3daa54c
Jeff Hall <hall.jeffw@gmail.com>
2020-04-27 19:09:14 -0400

    Server stubs completed.

---
+++ src/server/lib/module.js
@@ -0,0 +1,14 @@
+import { Observable } from "rxjs";
+
+/*eslint no-unused-vars: "off"*/
+export class ModuleBase {
+  init$() {
+    return Observable.empty();
+  }
+
+  registerClient(client) {
+  }
+
+  clientRegistered(client) {
+  }
+}
--- src/server/server.js
+++ src/server/server.js
@@ -5,6 +5,9 @@ import socketIo from "socket.io";
 import chalk from "chalk";
 import { Observable } from "rxjs";
 import { ObservableSocket } from "shared/observable-socket";
+import { UsersModule } from "./modules/users";
+import { PlaylistModule } from "./modules/playlist";
+import { ChatModule } from "./modules/chat";
 
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
@@ -53,8 +56,17 @@ app.get("/", (req, res) => {
 })
 
 // ------------------------
-// Modules
+// Services
+const videoServices = [];
+const playlistRepository = {};
 
+
+// ------------------------
+// Modules
+const users = new UsersModule(io);
+const chat = new ChatModule(io, users);
+const playlist = new PlaylistModule(io, users, playlistRepository, videoServices);
+const modules = [users, chat, playlist];
 
 // ------------------------
 // Socket
@@ -62,10 +74,13 @@ io.on("connection", socket => {
   console.log(`Got connection from ${socket.request.connection.remoteAddress}.`);
 
   const client = new ObservableSocket(socket);
-  client.onAction("login", creds => {
-    //throw new Error("Tits!");
-    return Observable.of(`USER: ${creds.username}`).delay(3000);
-  });
+  for (let mod of modules) {
+    mod.registerClient(client);
+  }
+
+  for (let mod of modules) {
+    mod.clientRegistered(client);
+  }
 });
 
 // ------------------------
@@ -77,4 +92,13 @@ function startServer() {
   });
 }
 
-startServer();
+Observable.merge(...modules.map(m => m.init$()))
+  .subscribe({
+    complete() {
+      startServer();
+    },
+
+    error(error) {
+      console.error(`Could not init module: ${error.stack || error}.`);
+    }
+  });
